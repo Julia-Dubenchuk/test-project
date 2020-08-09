3ebf7c5f9291fb74366f076b1c7b5679ab0b5adb
Yulia Dubenchuk <dubenchuk6@gmail.com>
2020-08-09 20:17:56 +0300

    Update SortableStressTest.tsx

--- src/components/SortableStressTest.tsx
+++ src/components/SortableStressTest.tsx
@@ -4,7 +4,7 @@ import Container from './Container';
 export const SortableStressTest: React.FC = () => {
   // Avoid rendering on server because the big data list is generated
   const [shouldRender, setShouldRender] = useState(false);
-  useEffect(() => setShouldRender(true));
+  useEffect(() => setShouldRender(true), []);
   return <>{shouldRender && <Container />}</>;
 };
 
